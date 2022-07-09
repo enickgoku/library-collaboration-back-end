99c2780420f5d213b408c0d8c4c814ac21cd2650
Jon Belaire <37884708+jeriale@users.noreply.github.com>
2022-07-09 13:03:45 -0400

    Server boilerplate

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv/config')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send({ success: true }))
+
+app.use((req, res) => res.status(404).send({ error: 'Not found' }))
+
+const port = process.env.PORT || 3000
+
+app.listen(port)
+
+console.info('Server running on port', port)
